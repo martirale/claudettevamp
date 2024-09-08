9a65117b4720f77d5496bd70b01d16224283cea7
Alejandro Martir <hola@alemartir.com>
2024-09-08 00:19:58 -0600

    Change header fixed position

--- frontend/src/components/ui/Header.js
+++ frontend/src/components/ui/Header.js
@@ -107,7 +107,7 @@ const Header = () => {
   if (isHomePage) return null;
 
   return (
-    <header className="fixed top-0 left-0 w-full gradient-header border-b-scarlet border-b-[1px] z-50">
+    <header className="top-0 left-0 w-full gradient-header border-b-scarlet border-b-[1px] z-50">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20 md:h-24">
           {isMobile ? renderMobileMenu() : renderDesktopMenu()}
