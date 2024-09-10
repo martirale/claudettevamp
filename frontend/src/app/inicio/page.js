bd28bd893f356067940c19c357b144a9fe0b5f5f
Alejandro Martir <hola@alemartir.com>
2024-09-10 02:30:51 -0600

    Add book slideshow

--- frontend/src/app/inicio/page.js
+++ frontend/src/app/inicio/page.js
@@ -1,4 +1,8 @@
+import BookSlideshow from "@/components/BookSlideshow";
 import { getMetadata } from "@/lib/getMetadata";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export const metadata = getMetadata({
   title: "Claudette Vamp",
@@ -13,6 +17,12 @@ export default function Home() {
   return (
     <div>
       <h1>Claudette Vamp</h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-12 gap-8">
+        <div className="col-span-12 md:col-span-6">
+          <BookSlideshow />
+        </div>
+      </section>
     </div>
   );
 }
