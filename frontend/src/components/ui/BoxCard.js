e2d1e0a0de30d353c2d03a798e965ad938fcb376
Alejandro Martir <hola@alemartir.com>
2024-09-09 23:49:24 -0600

    Update BoxCard component

--- frontend/src/components/ui/BoxCard.js
+++ frontend/src/components/ui/BoxCard.js
@@ -1,6 +1,8 @@
-export default function BoxCard({ children }) {
+export default function BoxCard({ children, className = "" }) {
   return (
-    <div className="border border-black px-8 py-5 transition-colors hover:border-scarlet">
+    <div
+      className={`border border-black px-8 py-5 transition-colors hover:border-scarlet ${className}`}
+    >
       {children}
     </div>
   );
