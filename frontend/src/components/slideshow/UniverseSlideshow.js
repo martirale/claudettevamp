655e4a019a39e7cae1adb3a1710796799e3d4d73
Alejandro Martir <hola@alemartir.com>
2024-09-10 14:00:42 -0600

    Create universe slideshow

---
+++ frontend/src/components/slideshow/UniverseSlideshow.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import { register } from "swiper/element/bundle";
+
+export default function UniverseSlideshow() {
+  return (
+    <div className="w-full max-w-9xl mx-auto">
+      <div></div>
+    </div>
+  );
+}
