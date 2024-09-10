f2f7d4b8563d8d94720d61172ba77963738ef6f8
Alejandro Martir <hola@alemartir.com>
2024-09-10 12:07:36 -0600

    Add top padding

--- frontend/src/components/hero/HeroBooks.js
+++ frontend/src/components/hero/HeroBooks.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function HeroBooks() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-12 gap-8">
+    <section className="grid grid-cols-1 md:grid-cols-12 gap-8 pt-4">
       <div className="col-span-12 md:col-span-6 flex justify-center">
         <Image
           src="/books/claudette-caida-ascenso.png"
