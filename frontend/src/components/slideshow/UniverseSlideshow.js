f05fac9ac81aa8c9269e5be2f67af203eb27f9da
Alejandro Martir <hola@alemartir.com>
2024-09-10 16:38:23 -0600

    Customize universe section slideshow

--- frontend/src/components/slideshow/UniverseSlideshow.js
+++ frontend/src/components/slideshow/UniverseSlideshow.js
@@ -1,9 +1,77 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar, Autoplay } from "swiper/modules";
+import "swiper/css";
 import styles from "./universe.module.css";
+import Link from "next/link";
+
+const slides = [
+  {
+    image: "/sample-v.png",
+    url: "#",
+    title: "Slide 1",
+  },
+  {
+    image: "/sample-v.png",
+    url: "#",
+    title: "Slide 2",
+  },
+  {
+    image: "/sample-v.png",
+    url: "#",
+    title: "Slide 3",
+  },
+  {
+    image: "/sample-v.png",
+    url: "#",
+    title: "Slide 4",
+  },
+  {
+    image: "/sample-v.png",
+    url: "#",
+    title: "Slide 5",
+  },
+];
 
 export default function UniverseSlideshow() {
   return (
-    <div className="w-full max-w-9xl mx-auto">
-      <div></div>
+    <div>
+      <h2 className="font-LeMurmure text-scarlet text-center text-3xl md:text-4xl mb-8">
+        Descubre el universo de Claudette
+      </h2>
+
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        scrollbar={{ draggable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        breakpoints={{
+          640: {
+            slidesPerView: 4,
+          },
+        }}
+        className={styles.swiper}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <Link href={slide.url}>
+              <div className={styles.hexagon}>
+                <img
+                  src={slide.image}
+                  alt={slide.title}
+                  className={styles.slideImage}
+                />
+              </div>
+              <h3 className={`${styles.slideTitle} font-LeMurmure text-3xl`}>
+                {slide.title}
+              </h3>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
