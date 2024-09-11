2608998df0808bfe457921bc357185d2e610f8ff
Alejandro Martir <hola@alemartir.com>
2024-09-10 18:31:05 -0600

    Add heading page and container structure

--- frontend/src/app/autor/page.js
+++ frontend/src/app/autor/page.js
@@ -13,6 +13,14 @@ export default function Author() {
   return (
     <div>
       <h1>Alejandro Mártir</h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-12 gap-8">
+        <div className="col-span-12 md:col-span-8">
+          <h2 className="font-LeMurmure text-scarlet text-6xl md:text-7xl mb-16">
+            Alejandro Mártir
+          </h2>
+        </div>
+      </section>
     </div>
   );
 }
