64966a2c6e0492343cf0121e8cf21b8d41d9ae1a
Alejandro Martir <hola@alemartir.com>
2024-09-11 15:56:48 -0600

    Create books page

---
+++ frontend/src/app/libros/page.js
@@ -0,0 +1,91 @@
+import { getMetadata } from "@/lib/getMetadata";
+import Link from "next/link";
+import Image from "next/image";
+import DividerLogo from "@/components/ui/DividerLogo";
+
+export const metadata = getMetadata({
+  title: "Libros — Claudette Vamp",
+  description:
+    "Claudette Vamp es una trilogía de novelas cortas de fantasía oscura y romance escritas por Alejandro Mártir.",
+  url: "https://claudettevamp.com/libros",
+  image: "https://claudettevamp.com/claudette-cover.webp",
+  canonical: "https://claudettevamp.com/libros",
+});
+
+async function fetchAllBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/books`);
+  if (!response.ok) {
+    throw new Error("Error al obtener los libros");
+  }
+  return response.json();
+}
+
+export default async function Universe() {
+  let books = [];
+
+  try {
+    books = await fetchAllBooks();
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Filtrar libros por serie
+  const booksCaida = books.filter((book) => book.series === "Caída y Ascenso");
+  const booksRelatos = books.filter(
+    (book) => book.series === "Claudette Vamp: Relatos"
+  );
+
+  return (
+    <div>
+      <h1>Libros</h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-12 gap-8">
+        <div className="col-span-12">
+          <div className="mb-32">
+            <h2 className="font-LeMurmure text-scarlet text-center text-6xl md:text-7xl">
+              Caída y Ascenso
+            </h2>
+
+            <DividerLogo className="my-8" />
+
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+              {booksCaida.map((book, index) => (
+                <Link key={index} href={`/libro/${book.slug}`}>
+                  <Image
+                    src={book.cover}
+                    alt={book.title}
+                    width={400}
+                    height={640}
+                  />
+                  <h4 className="text-center mt-4">{book.title}</h4>
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h2 className="font-LeMurmure text-scarlet text-center text-6xl md:text-7xl">
+              Relatos
+            </h2>
+
+            <DividerLogo className="my-8" />
+
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+              {booksRelatos.map((book, index) => (
+                <Link key={index} href={`/libro/${book.slug}`}>
+                  <Image
+                    src={book.cover}
+                    alt={book.title}
+                    width={400}
+                    height={640}
+                  />
+                  <h4 className="text-center mt-4">{book.title}</h4>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
