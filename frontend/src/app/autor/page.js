cf5420f68e0813e88f7b196e69958c39ac51e9c6
Alejandro Martir <hola@alemartir.com>
2024-09-10 18:40:59 -0600

    Add author about description

--- frontend/src/app/autor/page.js
+++ frontend/src/app/autor/page.js
@@ -1,4 +1,5 @@
 import { getMetadata } from "@/lib/getMetadata";
+import Link from "next/link";
 
 export const metadata = getMetadata({
   title: " Alejandro Mártir — Claudette Vamp",
@@ -19,6 +20,30 @@ export default function Author() {
           <h2 className="font-LeMurmure text-scarlet text-6xl md:text-7xl mb-16">
             Alejandro Mártir
           </h2>
+
+          <p className="mb-8">
+            Alejandro es alguien muy curioso, creativo y autodidacta. Desde muy
+            joven ha tenido una gran fascinación por la fantasía, la mitología y
+            el <span className="italic">sci-fi.</span> Al tener una mente muy
+            imaginativa, suele perderse en sus pensamientos y películas mentales
+            que va creando en su cabeza a lo largo del día. De estas "películas"
+            mentales surgió la idea que ahora es{" "}
+            <Link
+              href="https://www.amazon.com/dp/B0BNZMWXYT"
+              target="_blank"
+              rel="noopener"
+              className="text-scarlet hover:underline"
+            >
+              Claudette, caída y ascenso
+            </Link>
+            .
+          </p>
+
+          <p>
+            Es alguien que ama lo que hace, por lo que suele entregarse de lleno
+            en las cosas con las que se involucra. Le fascina el diseño, el
+            cine, el café y cualquier prenda de color negro.
+          </p>
         </div>
       </section>
     </div>
