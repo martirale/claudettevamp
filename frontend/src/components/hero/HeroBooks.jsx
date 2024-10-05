446f0d9688aeae630c521f759981dee2772827d4
Alejandro Martir <hola@alemartir.com>
2024-10-05 15:39:02 -0600

    Remove bottom padding

--- frontend/src/components/hero/HeroBooks.jsx
+++ frontend/src/components/hero/HeroBooks.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function HeroBooks() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-12 gap-8 pt-8 pb-4">
+    <section className="grid grid-cols-1 md:grid-cols-12 gap-8 pt-8">
       <div className="col-span-12 md:col-span-6 flex justify-center">
         <Image
           src="/books/claudette-caida-ascenso.webp"
