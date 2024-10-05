0ec1baf49cd04df8cb524cd7c6061c8092ea3a87
Alejandro Martir <hola@alemartir.com>
2024-10-05 15:15:17 -0600

    Create 'mythology' page

---
+++ frontend/src/app/mitologia/page.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { getMetadata } from "@/lib/getMetadata";
+import Link from "next/link";
+import DividerLogo from "@/components/ui/DividerLogo";
+import HexagonCard from "@/components/hexagon/HexagonCard";
+
+export const metadata = getMetadata({
+  title: "Mitología — Claudette Vamp",
+  description:
+    "Claudette Vamp es una trilogía de novelas cortas de fantasía oscura y romance escritas por Alejandro Mártir.",
+  url: "https://claudettevamp.com/mitologia",
+  image: "https://claudettevamp.com/claudette-cover.webp",
+  canonical: "https://claudettevamp.com/mitologia",
+});
+
+async function fetchAllCharacters() {
+  const apiSecretKey = process.env.NEXT_PUBLIC_API_SECRET_KEY;
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/characters`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-Key": apiSecretKey,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Error al obtener los personajes");
+  }
+  return res.json();
+}
+
+export default async function MythoPage() {
+  let characters = [];
+
+  try {
+    characters = await fetchAllCharacters();
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Filtrar personajes por tipo
+  const charactersProtas = characters.filter(
+    (character) => character.type === "Protagonista"
+  );
+
+  return (
+    <div>
+      <h1>Mitología</h1>
+
+      <section className="grid grid-cols-1 md:grid-cols-12">
+        <div className="col-span-12">
+          <div>
+            <h2 className="font-LeMurmure text-scarlet text-center text-6xl md:text-7xl">
+              Mitología
+            </h2>
+
+            <DividerLogo className="my-8" />
+
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8">
+              {charactersProtas.map((character, index) => (
+                <Link key={index} href={`/personaje/${character.slug}`}>
+                  <HexagonCard
+                    source={character.avatar}
+                    alternative={character.name}
+                  />
+                  <h4 className="text-center my-4">{character.name}</h4>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
