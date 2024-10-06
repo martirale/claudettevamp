267211dd49222e84d28f02c60f92d615ca3e0cb5
Alejandro Martir <hola@alemartir.com>
2024-10-05 21:48:51 -0600

    Change page name from Mythology to Species

--- frontend/src/app/especies/page.jsx
+++ frontend/src/app/especies/page.jsx
@@ -5,18 +5,18 @@ import DividerLogo from "@/components/ui/DividerLogo";
 import HexagonCard from "@/components/hexagon/HexagonCard";
 
 export const metadata = getMetadata({
-  title: "Mitología — Claudette Vamp",
+  title: "Especies — Claudette Vamp",
   description:
     "Claudette Vamp es una trilogía de novelas cortas de fantasía oscura y romance escritas por Alejandro Mártir.",
-  url: "https://claudettevamp.com/mitologia",
+  url: "https://claudettevamp.com/especies",
   image: "https://claudettevamp.com/claudette-cover.webp",
-  canonical: "https://claudettevamp.com/mitologia",
+  canonical: "https://claudettevamp.com/especies",
 });
 
-async function fetchAllCharacters() {
+async function fetchAllSpecies() {
   const apiSecretKey = process.env.NEXT_PUBLIC_API_SECRET_KEY;
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/characters`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/species`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -26,46 +26,41 @@ async function fetchAllCharacters() {
   });
 
   if (!res.ok) {
-    throw new Error("Error al obtener los personajes");
+    throw new Error("Error al obtener las especies");
   }
   return res.json();
 }
 
-export default async function MythoPage() {
-  let characters = [];
+export default async function SpeciesPage() {
+  let species = [];
 
   try {
-    characters = await fetchAllCharacters();
+    species = await fetchAllSpecies();
   } catch (error) {
     console.error(error);
   }
 
-  // Filtrar personajes por tipo
-  const charactersProtas = characters.filter(
-    (character) => character.type === "Protagonista"
-  );
-
   return (
     <div>
-      <h1>Mitología</h1>
+      <h1>Especies</h1>
 
       <section className="grid grid-cols-1 md:grid-cols-12">
         <div className="col-span-12">
           <div>
             <h2 className="font-LeMurmure text-scarlet text-center text-6xl md:text-7xl">
-              Mitología
+              Especies
             </h2>
 
             <DividerLogo className="my-8" />
 
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8">
-              {charactersProtas.map((character, index) => (
-                <Link key={index} href={`/personaje/${character.slug}`}>
+              {species.map((specie, index) => (
+                <Link key={index} href={`/especies/${specie.slug}`}>
                   <HexagonCard
-                    source={character.avatar}
-                    alternative={character.name}
+                    source={specie.avatar}
+                    alternative={specie.name}
                   />
-                  <h4 className="text-center my-4">{character.name}</h4>
+                  <h4 className="text-center my-4">{specie.name}</h4>
                 </Link>
               ))}
             </div>
